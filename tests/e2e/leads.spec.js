c4d0e0d9afc12a3af1ab971c37d05076d76a39a0
barp14 <joaobarpy@gmail.com>
2025-02-23 01:58:15 -0300

    Adicionando primeiros testes da página de filmes, começando a consumir a API do site e o banco de dados nos testes, além de adicionar massas de testes estáticas com ajuda da intligência artificial

--- tests/e2e/leads.spec.js
+++ tests/e2e/leads.spec.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 import { LandingPage } from '../pages/LandingPage'
 import { Toast } from '../pages/Components'
 import { faker } from '@faker-js/faker';
@@ -22,10 +22,21 @@ test('cadastro lead', async ({ page }) => {
   await toast.haveText('Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!')
 })
 
-test('email existente', async ({ page }) => {
+test('email existente', async ({ page, request }) => {
+  const leadName = faker.person.fullName()
+  const leadEmail = faker.internet.email()
+
+  const newLead = await request.post('http://localhost:3333/leads', { // consumindo API
+    data: {
+      name: leadName,
+      email: leadEmail
+    }
+  })
+  expect(newLead.ok()).toBeTruthy()
+
   await landingPage.visit()
   await landingPage.openLeadModal()
-  await landingPage.submitLeadForm('João Barp', 'joaobarpy@gmail.com')
+  await landingPage.submitLeadForm(leadName, leadEmail)
   await toast.haveText('O endereço de e-mail fornecido já está registrado em nossa fila de espera.')
 })
 
---
+++ tests/e2e/movies.spec.js
@@ -0,0 +1,32 @@
+import { test } from '@playwright/test'
+
+const data = require('../support/fixtures/movies.json')
+const { executeSQL } = require('../support/database')
+
+import { LoginPage } from '../pages/LoginPage'
+import { MoviesPage } from '../pages/MoviesPage'
+import { Toast } from '../pages/Components'
+
+
+let loginPage
+let moviesPage
+let toast
+
+test.beforeEach(({ page }) => {
+  loginPage = new LoginPage(page)
+  moviesPage = new MoviesPage(page)
+  toast = new Toast(page)
+})
+
+test('cadastrar novo filme', async ({ page }) => {
+  const movie = data.create
+  await executeSQL(`DELETE from public.movies WHERE title = '${movie.title}';`)
+
+  await loginPage.visit()
+  await loginPage.submit('admin@zombieplus.com', 'pwd123')
+  await moviesPage.isLoggedIn()
+
+  await moviesPage.create(movie.title, movie.overview, movie.company, movie.release_year)
+
+  await toast.haveText(/Uhull/)
+})
